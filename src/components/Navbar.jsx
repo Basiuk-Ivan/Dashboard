5abbc0bc10972e9c6317878a0fa7d1c6bbfb84e1
IvanBasss <122455382+Basiuk-Ivan@users.noreply.github.com>
2023-09-09 13:45:51 +0300

    add favikon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -81,9 +81,9 @@ const Navbar = () => {
               alt="user-profile"
             />
             <p>
-              <span className="text-gray-400 text-14">Hi,</span>{' '}
+              {/* <span className="text-gray-400 text-14">Hi</span> */}
               <span className="text-gray-400 font-bold ml-1 text-14">
-                Michael
+                Hi there
               </span>
             </p>
             <MdKeyboardArrowDown className="text-gray-400 text-14" />
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { Link, NavLink } from 'react-router-dom';
-import { SiShopware } from 'react-icons/si';
+import {DiScala} from 'react-icons/di'
 import { MdOutlineCancel } from 'react-icons/md';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
@@ -31,7 +31,7 @@ const Sidebar = () => {
               onClick={handleCloseSideBar}
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
             >
-              <SiShopware /> <span>Shoppy</span>
+              <DiScala/> <span>DashMetrics</span>
             </Link>
             <TooltipComponent content="Menu" position="BottomCenter">
               <button
